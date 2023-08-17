ed21df320926cbc3707b47487b02fc1dd7630edd
Saba Madadi <sabamadadi9@gmail.com>
2023-08-17 11:47:28 +0330

    Update Knapsack Problem Algorithms.cpp

--- Knapsack Problem Algorithms.cpp
+++ Knapsack Problem Algorithms.cpp
@@ -1,31 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int knapsackDynamic(int W, const vector<int>& weights, const vector<int>& values, int n) {
-    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+struct Item {
+    int value;
+    int weight;
+};
 
-    for (int i = 1; i <= n; i++) {
-        for (int w = 1; w <= W; w++) {
-            if (weights[i - 1] <= w) {
-                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
-            } else {
-                dp[i][w] = dp[i - 1][w];
-            }
+bool compareItems(const Item& item1, const Item& item2) {
+    double ratio1 = static_cast<double>(item1.value) / item1.weight;
+    double ratio2 = static_cast<double>(item2.value) / item2.weight;
+    return ratio1 > ratio2;
+}
+
+int knapsackGreedy(int W, const vector<Item>& items) {
+    sort(items.begin(), items.end(), compareItems);
+
+    int totalValue = 0;
+    int totalWeight = 0;
+
+    for (const auto& item : items) {
+        if (totalWeight + item.weight <= W) {
+            totalValue += item.value;
+            totalWeight += item.weight;
+        } else {
+            int remainingWeight = W - totalWeight;
+            totalValue += static_cast<double>(remainingWeight) / item.weight * item.value;
+            break;
         }
     }
 
-    return dp[n][W];
+    return totalValue;
 }
 
 int main() {
     int W = 50;
-    vector<int> weights = {10, 20, 30};
-    vector<int> values = {60, 100, 120};
-    int n = weights.size();
+    vector<Item> items = {
+        {60, 10},
+        {100, 20},
+        {120, 30}
+    };
 
-    int max_value = knapsackDynamic(W, weights, values, n);
+    int max_value = knapsackGreedy(W, items);
 
     cout << "Maximum value that can be obtained: " << max_value << endl;
 
